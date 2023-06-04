541c48e4732dfe0826b9261d98d4519e9bea75a2
lucasil <lucas07.macedo@gmail.com>
2023-06-04 19:53:24 -0300

    organização e comentários

--- atividade_1a/atividade_1a.js
+++ atividade_1a/atividade_1a.js
@@ -1,18 +1,20 @@
-document.getElementById("botaoCalcula").addEventListener("click",() => {
+document.getElementById("botaoCalcula").addEventListener("click",() => { // chamar função anônima (arrow function), usada apenas para um elemento, sem necessidade de ser declarada
 
-    let num1 = parseFloat(prompt("Informe a primeira nota: "))
-    let num2 = parseFloat(prompt("Informe a segunda nota: "))
+    let num1 = parseFloat(prompt("Informe a sua primeira nota: "))
+    let num2 = parseFloat(prompt("Informe a sua segunda nota: "))
 
     let media = (num1 + num2) / 2
+
     let spanMedia = document.getElementById("spanMedia")
     let resultMedia = document.getElementById("resultMedia")
     
     if (media >= 6) {
         spanMedia.innerHTML = "APROVADO"
-        spanMedia.style.color = "blue"
+        spanMedia.style.color = "blue" // estilização da situação
     } else {
         spanMedia.innerHTML = "REPROVADO"
-        spanMedia.style.color = "red"
+        spanMedia.style.color = "red" // estilização da situação
     }
+
     resultMedia.innerHTML = media
 })
