1b9fc489bbaf037c151e331e06d3103c24c97111
lucasil <lucas07.macedo@gmail.com>
2023-06-06 20:57:04 -0300

    reformulação do js

--- atvidade_3/atividade_3.js
+++ atvidade_3/atividade_3.js
@@ -1,13 +1,16 @@
-function myFunction() {
+document.getElementById("botaoLista").addEventListener("click", () => {
 
-    let li = document.createElement("li");
-    
-    // Create a text node:
-    const textnode = document.createTextNode("Water");
-    
-    // Append the text node to the "li" node:
-    li.appendChild(textnode);
-    
-    // Append the "li" node to the list:
-    document.getElementById("myList").appendChild(li);
-    }
+    let li = document.createElement("li") // criação de um elemento (tag) <li></li>
+    let texto = document.getElementById("id_nome_lista").value
+
+    li.innerHTML = texto // inserção do texto dentro do elemento li - <li>"texto"</li>
+
+    let cor = document.querySelector('input[name="cor"]:checked').value 
+
+    li.style.color = cor // adicionar a cor ao estilo do texto do elemento li (value teve que ser digitado em inglês)
+
+    let ul = document.getElementById("lista")
+
+    ul.appendChild(li) // adicionar um elemento filho ao elemento ul
+
+})
